726c56138d9920879532de20fdc58c27c95d3b3f
zhongyao <zhongyao@alibaba-inc.com>
2020-02-20 21:44:48 +0800

    gradle 升级

--- app/src/main/java/com/hongri/nestedscrollview/MainActivity.java
+++ app/src/main/java/com/hongri/nestedscrollview/MainActivity.java
@@ -1,13 +1,16 @@
 package com.hongri.nestedscrollview;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * @author hongri
  */
 public class MainActivity extends AppCompatActivity {
 
+    //private RecycleListView
+    private RecyclerView mRV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
